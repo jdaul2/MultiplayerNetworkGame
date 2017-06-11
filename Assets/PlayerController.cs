b8660b8a5fcd5c3beca3725b83ebcca3649f21f7
jdaul <jeff@seven2.com>
2017-06-11 12:02:56 -0700

    PlayerController Update.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : NetworkBehaviour {
 	float cooldown = 0f;
 	float fireRate = .065f;
 	GameObject weapon;
+	float _timeSinceMouseActive = 0f;
+	float _timeMouseSleep = .5f;
 
 	// Use this for initialization
 	void Start () {
@@ -21,35 +23,70 @@ public class PlayerController : NetworkBehaviour {
 		Camera.main.transform.localPosition = Vector3.zero;
 		Camera.main.transform.localEulerAngles = Vector3.zero;
 	}
+
+	public bool isMouseMoving(){
+		return (Input.GetMouseButton(0) || (Input.GetAxis("Mouse X")!=0 && Input.GetAxis("Mouse Y")!=0));
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(!isLocalPlayer)return;
 
 		cooldown += Time.deltaTime;
+		_timeSinceMouseActive+=Time.deltaTime;
 
 		if(Input.GetAxis("Horizontal")!=0 || Input.GetAxis("Vertical")!=0){
 			transform.GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0f,Input.GetAxis("Vertical")*speed*Time.deltaTime));
 		}
 
-		if(!(Input.GetAxis("XBox-RightAnalogX")==0f && Input.GetAxis("XBox-RightAnalogY")==0f)){
+		//If mouse has moved for the last X seconds, remain visible
+		if(_timeSinceMouseActive<_timeMouseSleep){
 			if(!weapon.transform.GetChild(0).gameObject.activeInHierarchy){
 				CmdSetActive(true);
 			}
-			//weapon.transform.GetChild(0).gameObject.SetActive(true);
-			weapon.transform.localEulerAngles = new Vector3(0f,Mathf.LerpAngle(weapon.transform.localEulerAngles.y,-90f+Mathf.Atan2(Input.GetAxis("XBox-RightAnalogX"),-1*Input.GetAxis("XBox-RightAnalogY"))*Mathf.Rad2Deg,aimSpeed),0f);
+		}
+
+		if(!(Input.GetAxis("XBox-RightAnalogX")==0f && Input.GetAxis("XBox-RightAnalogY")==0f) || (isMouseMoving())){
+			if(!weapon.transform.GetChild(0).gameObject.activeInHierarchy){
+				CmdSetActive(true);
+			}
+			//Is Mouse
+			if(isMouseMoving()){
+				_timeSinceMouseActive = 0;
+				Vector3 mouse_pos = Input.mousePosition;
+				mouse_pos.z = 64.25f; //The distance between the camera and object
+				Vector3 object_pos = Camera.main.WorldToScreenPoint(weapon.transform.position);
+				mouse_pos.x = mouse_pos.x - object_pos.x;
+				mouse_pos.y = mouse_pos.y - object_pos.y;
+				float angle = -90f + Mathf.Atan2(mouse_pos.x, mouse_pos.y) * Mathf.Rad2Deg;
+
+				//weapon.transform.localEulerAngles = new Vector3(0f,angle,0f);
+				weapon.transform.localEulerAngles = new Vector3(0f,Mathf.LerpAngle(weapon.transform.localEulerAngles.y,angle,aimSpeed),0f);
+			}
+
+			//Is Controller
+			else{
+				weapon.transform.localEulerAngles = new Vector3(0f,Mathf.LerpAngle(weapon.transform.localEulerAngles.y,-90f+Mathf.Atan2(Input.GetAxis("XBox-RightAnalogX"),-1*Input.GetAxis("XBox-RightAnalogY"))*Mathf.Rad2Deg,aimSpeed),0f);
+			}
 		}
 		else{
-			if(weapon.transform.GetChild(0).gameObject.activeInHierarchy){
+			if(_timeSinceMouseActive>=_timeMouseSleep && weapon.transform.GetChild(0).gameObject.activeInHierarchy && !(Input.GetAxis("XBox-RTrigger")>=1 || Input.GetMouseButton(0))){
 				CmdSetActive(false);
 			}
 			//weapon.transform.GetChild(0).gameObject.SetActive(false);
 		}
-		if(Input.GetAxis("XBox-RTrigger")>=1){
+
+		//Different Control for Xbox and Mouse
+		if(Input.GetAxis("XBox-RTrigger")>=1 || Input.GetMouseButton(0)){
+			if(!weapon.transform.GetChild(0).gameObject.activeInHierarchy){
+				CmdSetActive(true);
+			}
 			if(cooldown>=fireRate){
 				CmdSpawn(weapon.transform.parent.GetComponent<Rigidbody>().velocity, weapon.transform.localEulerAngles.y);
 				cooldown = 0;
-				GamePad.SetVibration(PlayerIndex.One,1f,1f);
+				if(Input.GetAxis("XBox-RTrigger")>=1){
+					GamePad.SetVibration(PlayerIndex.One,1f,1f);
+				}
 			}
 			else{
 				GamePad.SetVibration(PlayerIndex.One,0f,0f);
